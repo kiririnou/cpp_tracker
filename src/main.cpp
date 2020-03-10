3fdc1715262f3df494260fd70d5c691342834977
kiririnou <kou.pass94@gmail.com>
2020-03-10 15:17:54 +0200

    Build for old server with threads. Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,47 @@
-﻿// #define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
-#include <chrono>
-#include <iomanip>
-#include <ctime>
+﻿#include <iostream>
 #include <string>
-#include <regex>
+#include <memory>
+#include <chrono>
+
+#include "mingw.thread.h"
 
 #include "utils.h"
-
-#include <curl/curl.h>
-
-#include <rapidjson/document.h>
-#include <rapidjson/prettywriter.h>
-#include <rapidjson/ostreamwrapper.h>
-
-#include "resource_usage.h"
+#include "process_activity.h"
 
 int main()
 {
-	std::cout << "Username: " << GetUsername() << std::endl;
-	std::cout << "HWID: " << GetHWID() << std::endl;
-	std::cout << "Memory in use: " << GetUsedMemory() << std::endl;
+	using namespace Utils;
 
-	// std::cout << "CPU: " << PrintCPUData(5) << std::endl;
-	std::cout << "Current time: " << GetNow() << std::endl;
+	std::string url = "http://192.168.1.116:5000/api/process_activity";
+	ProcessActivity old;
+	ProcessActivity current(GetHWID(), GetNow(), GetWindowName(), GetUsedMemory());
 
-    //SetConsoleOutputCP(CP_UTF8);
+	old = current;
 
-	#pragma region CURL test
-	// CURL* curl;
-	// CURLcode res;
-	// curl_global_init(CURL_GLOBAL_DEFAULT);
+	while (true)
+	{
+		current.SetHWID(GetHWID());
+		current.SetStartTime(GetNow());
+		current.SetProcessTitle(GetWindowName());
+		current.SetMemory(GetUsedMemory());
 
-	// curl = curl_easy_init();
-	// if (curl) 
-	// {
-	// 	curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/");
-	// 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
-	// 	curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-	// 	curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert.pem");
+		if (current.GetProcessTitle().empty())
+		{
+			current = old;
+			continue;
+		}
 
-	// 	res = curl_easy_perform(curl);
-	// 	if (res != CURLE_OK)
-	// 		std::cerr << "Curl failed. Code: " << res << std::endl;
-	// 	else 
-	// 		std::cout << "Curl succeeded\n";
+		if (current.GetProcessTitle() != old.GetProcessTitle())
+		{
+			old.SetEndTime(current.GetStartTime());
+			Post(url, old);
 
-	// 	curl_easy_cleanup(curl);
-	// }
-	// curl_global_cleanup();
-	#pragma endregion
-
-	#pragma region Serialization
-	using namespace rapidjson;
-
-	Resource_Usage res("id", "2020-02-02 10:10:10", "2020-02-02 20:20:20", "Process", 40);
-
-	StringBuffer buf;
-	PrettyWriter<StringBuffer> writer(buf);
-
-	writer.StartObject();
-	res.Serialize(writer);
-	writer.EndObject();
-
-	std::puts(buf.GetString());
-	#pragma endregion
-
-	#pragma region CURL POST
-	// CURL* curl;
-	// CURLcode result;
-
-	// curl = curl_easy_init();
-	// curl_slist* slist = NULL;
-	// slist = curl_slist_append(slist, "Content-Type: application/json");
-	// curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.116:5000/api/process_activity");
-	// curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
-	// curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf.GetString());
-	// curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, static_cast<long>(buf.GetLength()));
-
-	// result = curl_easy_perform(curl);
-	// if (result != CURLE_OK)
-	// 	std::cerr << "Curl failed. Code: " << result << std::endl;
-	// else
-	// 	std::cout << "Curl succeedded. Code: " << result << std::endl;
-	// curl_easy_cleanup(curl);
-	#pragma endregion
+			old = current;
+		}
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(500ms);
+	}
 
 	system("pause");
     return 0;
 }
-/*
-
-{
-	"hw_id": "$1",
-	"start": "$2",
-	"end": "$3",
-	"process_title": "$4",
-	"mem": $5
-}
-
-*/
--- src/utils.h
+++ src/utils.h
@@ -20,171 +20,217 @@
 #include <vector>
 #include <fstream>
 
-std::string ConvertUTF16ToUTF8(LPWSTR lpwstr)
-{
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-	return conv.to_bytes(std::wstring(lpwstr));
-}
+#include <curl/curl.h>
+#include "process_activity.h"
 
-std::string ConvertUTF16ToUTF8(std::wstring wstr)
-{
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-	return conv.to_bytes(wstr);
-}
+namespace Utils {
 
-std::string GetWindowName()
-{
-	auto hwnd = GetForegroundWindow();
-	int buf_length = GetWindowTextLength(hwnd) + 1;
-	LPWSTR title = new WCHAR[buf_length];
-	GetWindowTextW(hwnd, title, buf_length);
-
-	std::string str = ConvertUTF16ToUTF8(title);
-	delete[] title;
-
-	return str;
-}
-
-std::string GetUsername()
-{
-	const int SIZE = 255;
-	LPDWORD len = new DWORD[SIZE];
-	LPWSTR uname = new WCHAR[SIZE];
-	GetUserNameW(uname, len);
-
-	std::string str = ConvertUTF16ToUTF8(uname);
-	delete[] len;
-	delete[] uname;
-	return str;
-}
-
-std::string GetHWID()
-{
-	HKEY hKey = 0;
-	LPWSTR buff = new WCHAR[255];
-	DWORD dwType;
-	DWORD dwBufSize = 255;
-	const wchar_t* subkey = L"Software\\Microsoft\\Cryptography";
-	std::wstring strValueOfKey = L"bad";
-	
-	if (RegOpenKey(HKEY_LOCAL_MACHINE, subkey, &hKey) == ERROR_SUCCESS)
+	std::string ConvertUTF16ToUTF8(LPWSTR lpwstr)
 	{
-		dwType = REG_SZ;
-		if (RegQueryValueExW(hKey, L"MachineGuid", 0, &dwType, (LPBYTE)buff, &dwBufSize) == ERROR_SUCCESS)
+		std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+		return conv.to_bytes(std::wstring(lpwstr));
+	}
+
+	std::string ConvertUTF16ToUTF8(std::wstring wstr)
+	{
+		std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+		return conv.to_bytes(wstr);
+	}
+
+	std::string GetWindowName()
+	{
+		auto hwnd = GetForegroundWindow();
+		int buf_length = GetWindowTextLength(hwnd) + 1;
+		LPWSTR title = new WCHAR[buf_length];
+		GetWindowTextW(hwnd, title, buf_length);
+
+		std::string str = ConvertUTF16ToUTF8(title);
+		delete[] title;
+
+		return str;
+	}
+
+	std::string GetUsername()
+	{
+		const int SIZE = 255;
+		LPDWORD len = new DWORD[SIZE];
+		LPWSTR uname = new WCHAR[SIZE];
+		GetUserNameW(uname, len);
+
+		std::string str = ConvertUTF16ToUTF8(uname);
+		delete[] len;
+		delete[] uname;
+		return str;
+	}
+
+	std::string GetHWID()
+	{
+		HKEY hKey = 0;
+		LPWSTR buff = new WCHAR[255];
+		DWORD dwType;
+		DWORD dwBufSize = 255;
+		const char* subkey = "Software\\Microsoft\\Cryptography";
+		std::wstring strValueOfKey = L"bad";
+
+		if (RegOpenKey(HKEY_LOCAL_MACHINE, subkey, &hKey) == ERROR_SUCCESS)
 		{
-			strValueOfKey = buff;
+			dwType = REG_SZ;
+			if (RegQueryValueExW(hKey, L"MachineGuid", 0, &dwType, (LPBYTE)buff, &dwBufSize) == ERROR_SUCCESS)
+			{
+				strValueOfKey = buff;
+			}
 		}
+		delete[]  buff;
+		return ConvertUTF16ToUTF8(strValueOfKey);
 	}
-	delete[]  buff;
-	return ConvertUTF16ToUTF8(strValueOfKey);
-}
 
-std::string GetNow()
-{
-	using namespace std::chrono;
-
-	auto time = system_clock::to_time_t(system_clock::now());
-	std::ostringstream oss;
-	oss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
-
-	return oss.str();
-}
-
-#define SystemProcessorPerformanceInformation 0x8
-#define SystemBasicInformation    0x0
-std::string PrintCPUData(int sec)
-{
-	typedef struct _SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION
+	std::string GetNow()
 	{
-		LARGE_INTEGER IdleTime;
-		LARGE_INTEGER KernelTime;
-		LARGE_INTEGER UserTime;
-		LARGE_INTEGER Reserved1[2];
-		ULONG Reserved2;
-	} SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION;
-	typedef struct _SYSTEM_BASIC_INFORMATION {
-		ULONG Reserved;
-		ULONG TimerResolution;
-		ULONG PageSize;
-		ULONG NumberOfPhysicalPages;
-		ULONG LowestPhysicalPageNumber;
-		ULONG HighestPhysicalPageNumber;
-		ULONG AllocationGranularity;
-		ULONG_PTR MinimumUserModeAddress;
-		ULONG_PTR MaximumUserModeAddress;
-		KAFFINITY ActiveProcessorsAffinityMask;
-		CCHAR NumberOfProcessors;
-	} SYSTEM_BASIC_INFORMATION, *PSYSTEM_BASIC_INFORMATION;
-	//	SYSTEM_INFO  sysinf;
+		using namespace std::chrono;
 
-	int nWaitSec = sec;
-	if (nWaitSec <= 0)
-		throw std::runtime_error("Waiting interval in seconds should be positive integer");
-	typedef DWORD(WINAPI * PNTQUERYSYSYTEMINFORMATION)(DWORD info_class, void *out, DWORD size, DWORD *out_size);
-	PNTQUERYSYSYTEMINFORMATION pNtQuerySystemInformation = NULL;
+		auto time = system_clock::to_time_t(system_clock::now());
+		std::ostringstream oss;
+		oss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
 
-	pNtQuerySystemInformation = (PNTQUERYSYSYTEMINFORMATION)GetProcAddress(GetModuleHandle(L"NTDLL.DLL"), "NtQuerySystemInformation");
-	SYSTEM_BASIC_INFORMATION sbi;
-	SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION * spi;
-
-	DWORD returnlength;
-	DWORD status = pNtQuerySystemInformation(SystemBasicInformation, &sbi,
-		sizeof(SYSTEM_BASIC_INFORMATION), &returnlength);
-
-	spi = new SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[sbi.NumberOfProcessors];
-
-	memset(spi, 0, sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION)*sbi.NumberOfProcessors);
-
-	status = pNtQuerySystemInformation(SystemProcessorPerformanceInformation, spi,
-		(sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION)*sbi.NumberOfProcessors), &returnlength);
-	int numberOfCores = returnlength / sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION);
-
-	static ULARGE_INTEGER  ul_sys_idle_old[32];
-	static ULARGE_INTEGER  ul_sys_kernel_old[32];
-	static ULARGE_INTEGER  ul_sys_user_old[32];
-
-	float          usage = 0;
-	float          usageAccum = 0;
-
-	Sleep(nWaitSec * 1000);
-	status = pNtQuerySystemInformation(SystemProcessorPerformanceInformation, spi,
-		(sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION)*numberOfCores), &returnlength);
-	usageAccum = 0;
-	for (int ii = 0; ii < numberOfCores; ii++)
-	{
-		ULARGE_INTEGER        ul_sys_idle;
-		ULARGE_INTEGER        ul_sys_kernel;
-		ULARGE_INTEGER        ul_sys_user;
-
-		ul_sys_idle.QuadPart = spi[ii].IdleTime.QuadPart;
-		ul_sys_kernel.QuadPart = spi[ii].KernelTime.QuadPart;
-		ul_sys_user.QuadPart = spi[ii].UserTime.QuadPart;
-
-		ULONGLONG kernelTime = (ul_sys_kernel.QuadPart - ul_sys_kernel_old[ii].QuadPart);
-		ULONGLONG usertime = (ul_sys_user.QuadPart - ul_sys_user_old[ii].QuadPart);
-		ULONGLONG idletime = (ul_sys_idle.QuadPart - ul_sys_idle_old[ii].QuadPart);
-
-		ULONGLONG proctime = kernelTime + usertime - idletime;
-
-		ULONGLONG totaltime = kernelTime + usertime;
-
-		usage = (float)(proctime * 100) / totaltime;
-		usageAccum += usage;
+		return oss.str();
 	}
-	usageAccum /= numberOfCores;
-	delete[] spi;
 
-	return std::to_string(usageAccum);
-}
+	#define SystemProcessorPerformanceInformation 0x8
+	#define SystemBasicInformation    0x0
+	std::string PrintCPUData(int sec)
+	{
+		typedef struct _SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION
+		{
+			LARGE_INTEGER IdleTime;
+			LARGE_INTEGER KernelTime;
+			LARGE_INTEGER UserTime;
+			LARGE_INTEGER Reserved1[2];
+			ULONG Reserved2;
+		} SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION;
+		typedef struct _SYSTEM_BASIC_INFORMATION {
+			ULONG Reserved;
+			ULONG TimerResolution;
+			ULONG PageSize;
+			ULONG NumberOfPhysicalPages;
+			ULONG LowestPhysicalPageNumber;
+			ULONG HighestPhysicalPageNumber;
+			ULONG AllocationGranularity;
+			ULONG_PTR MinimumUserModeAddress;
+			ULONG_PTR MaximumUserModeAddress;
+			KAFFINITY ActiveProcessorsAffinityMask;
+			CCHAR NumberOfProcessors;
+		} SYSTEM_BASIC_INFORMATION, *PSYSTEM_BASIC_INFORMATION;
+		//	SYSTEM_INFO  sysinf;
 
-std::string GetUsedMemory()
-{
-	MEMORYSTATUSEX memStatusEx;
-	memStatusEx.dwLength = sizeof(memStatusEx);
-	GlobalMemoryStatusEx(&memStatusEx);
+		int nWaitSec = sec;
+		if (nWaitSec <= 0)
+			throw std::runtime_error("Waiting interval in seconds should be positive integer");
+		typedef DWORD(WINAPI * PNTQUERYSYSYTEMINFORMATION)(DWORD info_class, void *out, DWORD size, DWORD *out_size);
+		PNTQUERYSYSYTEMINFORMATION pNtQuerySystemInformation = NULL;
 
-	// need convert in to int (uint)
-	auto mem = ((memStatusEx.ullTotalPhys - memStatusEx.ullAvailPhys) * 100 / memStatusEx.ullTotalPhys);
-	std::wstring wstr = std::to_wstring(mem);
-	return ConvertUTF16ToUTF8(wstr);
+		pNtQuerySystemInformation = (PNTQUERYSYSYTEMINFORMATION)GetProcAddress(GetModuleHandle("NTDLL.DLL"), "NtQuerySystemInformation");
+		SYSTEM_BASIC_INFORMATION sbi;
+		SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION * spi;
+
+		DWORD returnlength;
+		DWORD status = pNtQuerySystemInformation(SystemBasicInformation, &sbi,
+			sizeof(SYSTEM_BASIC_INFORMATION), &returnlength);
+
+		spi = new SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION[sbi.NumberOfProcessors];
+
+		memset(spi, 0, sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION)*sbi.NumberOfProcessors);
+
+		status = pNtQuerySystemInformation(SystemProcessorPerformanceInformation, spi,
+			(sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION)*sbi.NumberOfProcessors), &returnlength);
+		int numberOfCores = returnlength / sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION);
+
+		static ULARGE_INTEGER  ul_sys_idle_old[32];
+		static ULARGE_INTEGER  ul_sys_kernel_old[32];
+		static ULARGE_INTEGER  ul_sys_user_old[32];
+
+		float          usage = 0;
+		float          usageAccum = 0;
+
+		Sleep(nWaitSec * 1000);
+		status = pNtQuerySystemInformation(SystemProcessorPerformanceInformation, spi,
+			(sizeof(SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION)*numberOfCores), &returnlength);
+		usageAccum = 0;
+		for (int ii = 0; ii < numberOfCores; ii++)
+		{
+			ULARGE_INTEGER        ul_sys_idle;
+			ULARGE_INTEGER        ul_sys_kernel;
+			ULARGE_INTEGER        ul_sys_user;
+
+			ul_sys_idle.QuadPart = spi[ii].IdleTime.QuadPart;
+			ul_sys_kernel.QuadPart = spi[ii].KernelTime.QuadPart;
+			ul_sys_user.QuadPart = spi[ii].UserTime.QuadPart;
+
+			ULONGLONG kernelTime = (ul_sys_kernel.QuadPart - ul_sys_kernel_old[ii].QuadPart);
+			ULONGLONG usertime = (ul_sys_user.QuadPart - ul_sys_user_old[ii].QuadPart);
+			ULONGLONG idletime = (ul_sys_idle.QuadPart - ul_sys_idle_old[ii].QuadPart);
+
+			ULONGLONG proctime = kernelTime + usertime - idletime;
+
+			ULONGLONG totaltime = kernelTime + usertime;
+
+			usage = (float)(proctime * 100) / totaltime;
+			usageAccum += usage;
+		}
+		usageAccum /= numberOfCores;
+		delete[] spi;
+
+		return std::to_string(usageAccum);
+	}
+
+	int GetUsedMemory()
+	{
+		MEMORYSTATUSEX memStatusEx;
+		memStatusEx.dwLength = sizeof(memStatusEx);
+		GlobalMemoryStatusEx(&memStatusEx);
+
+		// need convert in to int (uint)
+		auto mem = ((memStatusEx.ullTotalPhys - memStatusEx.ullAvailPhys) * 100 / memStatusEx.ullTotalPhys);
+		// std::wstring wstr = std::to_wstring(mem);
+		return static_cast<int>(mem);
+	}
+
+	std::string Jsonify(ProcessActivity res)
+	{
+		using namespace rapidjson;
+		
+		StringBuffer strbuf;
+		PrettyWriter<StringBuffer> writer(strbuf);
+	
+		writer.StartObject();
+		res.Serialize(writer);
+		writer.EndObject();
+	
+		return strbuf.GetString();
+	}
+	
+	int Post(const std::string& url, ProcessActivity res)
+	{
+		CURL* curl;
+		CURLcode resCode;
+		curl_slist* slist = nullptr;
+	
+		curl = curl_easy_init();
+		slist = curl_slist_append(slist, "Content-Type: application/json");
+	
+		std::string json = Jsonify(res);
+	
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json.c_str());
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, static_cast<long>(json.length()));
+	
+		std::cerr << json << std::endl;
+
+		resCode = curl_easy_perform(curl);
+		if (resCode != CURLE_OK)
+			throw std::runtime_error("Curl failed. Code: " + resCode);
+	
+		curl_easy_cleanup(curl);
+	}
+
 }
