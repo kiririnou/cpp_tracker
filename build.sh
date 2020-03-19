7e72bffb8d4dcb23b95953b9701ccd7e808355ac
Kou Pass <kou.pass94@gmail.com>
2020-03-19 16:04:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-rm -rf build/*
+#rm -rf build/*
+CC=x86_64-w64-mingw32-gcc
+CXX=x86_64-w64-mingw32-g++
 cmake -B build/ -DCMAKE_TOOLCHAIN_FILE=scripts/win64.cmake
 make -C build/ -j2
 #cp /cpp_tracker.zip /dist/cpp_tracker.zip
